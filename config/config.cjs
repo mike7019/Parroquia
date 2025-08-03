6966a6225d3bae0ff45bb8363b2b68896f909c6f
mike7019 <amph7019@gmail.com>
2025-08-02 23:27:38 -0500

    update

--- config/config.cjs
+++ config/config.cjs
@@ -26,11 +26,11 @@ module.exports = {
     logging: false
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: process.env.DB_USER || 'parroquia_user',
+    password: process.env.DB_PASSWORD || 'admin',
+    database: process.env.DB_NAME || 'parroquia_db',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: false,
     pool: {
